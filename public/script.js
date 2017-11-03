43863dd0c6fbdf880079278777f643148315613b
AnhDT <anhdt@rikkeisoft.com>
2017-11-03 16:00:46 +0700

    error with format document feature vs code

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('It works.');
+new Vue({
+  el: '#app',
+  data:{
+    total: 100,
+    items: [
+      { title: "harrypotter 1", author:"JK Rowling" },
+      { title: "So do", author:"Vu trong phung" },
+    ],
+    cart: []
+  },
+  methods: {
+    addItem (index){
+      this.total+=10;
+      this.cart.push(this.items[index]);
+    }
+  }
+})
